5fb8a29744ed674c8979ac50faf9d5f66583f771
Anthony Canet <tony264269@gmail.com>
2023-11-23 23:56:24 +0100

    exercice 7

---
+++ js/Exercice_7.js
@@ -0,0 +1,10 @@
+let pointure = (41);
+let dateDeNaissance = (1986);
+
+function resultat(pointure, dateDeNaissance){
+
+    return( (pointure*2)+(5*50)-dateDeNaissance+1776)
+}
+
+console.log(resultat(pointure, dateDeNaissance));
+alert(resultat(pointure, dateDeNaissance));
